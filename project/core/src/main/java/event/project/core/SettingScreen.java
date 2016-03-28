42e38358aa5be307b411d1f69f7e11ffc5429f81
Aphisiit <aphisiit086757@hotmail.com>
2016-03-29 00:35:29 +0700

    test import sound object.

--- project/core/src/main/java/event/project/core/SettingScreen.java
+++ project/core/src/main/java/event/project/core/SettingScreen.java
@@ -5,8 +5,12 @@ import static playn.core.PlayN.*;
 import playn.core.Image;
 import playn.core.ImageLayer;
 import playn.core.Mouse;
+import playn.core.Sound;
 import tripleplay.game.Screen;
 import tripleplay.game.ScreenStack;
+
+import java.util.Stack;
+
 /**
  * Created by aphis on 24-Mar-16.
  */
@@ -15,15 +19,16 @@ public class SettingScreen extends Screen{
     private final ScreenStack ss;
     private final ImageLayer settingBG;
     private final ImageLayer backButton;
-    private final ImageLayer Sound;
+    private final ImageLayer SoundLayer;
     private final ImageLayer unSound;
-    private Boolean checkSound;
-
-//    Private Root root;
+    private final Sound sound;
 
     public SettingScreen(final ScreenStack ss){
         this.ss = ss;
-        checkSound = true;
+
+        sound = assets().getSound("sound/lean_on");
+        sound.setLooping(true);
+        sound.play();
 
         Image bg = assets().getImage("Images/settingBG.png");
         settingBG = graphics().createImageLayer(bg);
@@ -32,12 +37,12 @@ public class SettingScreen extends Screen{
         backButton = graphics().createImageLayer(backImage);
 
         Image SoundImage = assets().getImage("Images/unmute.png");
-        Sound = graphics().createImageLayer(SoundImage);
+        SoundLayer = graphics().createImageLayer(SoundImage);
 
         final Image unSoundImage = assets().getImage("Images/mute.png");
         unSound = graphics().createImageLayer(unSoundImage);
 
-        Sound.setTranslation(75,75);
+        SoundLayer.setTranslation(75,75);
         unSound.setTranslation(75,75);
 
         backButton.setTranslation(10,10);
@@ -47,17 +52,19 @@ public class SettingScreen extends Screen{
                 ss.remove(ss.top());
             }
         });
-        Sound.addListener(new Mouse.LayerAdapter(){
+        SoundLayer.addListener(new Mouse.LayerAdapter(){
             @Override
             public void onMouseUp(Mouse.ButtonEvent event) {
-                layer.remove(Sound);
+                sound.setVolume(0.0f);
+                layer.remove(SoundLayer);
                 layer.add(unSound);
             }
         });
         unSound.addListener(new Mouse.LayerAdapter(){
            public void onMouseUp(Mouse.ButtonEvent event){
+               sound.setVolume(1.0f);
                layer.remove(unSound);
-               layer.add(Sound);
+               layer.add(SoundLayer);
            }
         });
     }
@@ -67,8 +74,7 @@ public class SettingScreen extends Screen{
         super.wasShown();
         this.layer.add(settingBG);
         this.layer.add(backButton);
-        this.layer.add(Sound);
-        this.layer.add(unSound);
+        this.layer.add(SoundLayer);
     }
 }
 
