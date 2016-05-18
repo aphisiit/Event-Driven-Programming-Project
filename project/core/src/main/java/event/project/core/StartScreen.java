a280c2d4edf67d4faf504c4c6a4b9a819b6e1d19
Aphisiit <aphisiit086757@hotmail.com>
2016-05-18 15:37:32 +0700

    Added Sequence Screen

--- project/core/src/main/java/event/project/core/StartScreen.java
+++ project/core/src/main/java/event/project/core/StartScreen.java
@@ -13,41 +13,39 @@ public class StartScreen extends Screen {
 
     private final ScreenStack ss;
 //    private final SettingScreen settingScreen;
-    //private final GameScreen gameScreen;
     private final ImageLayer startLayer;
     private final ImageLayer startButtonLayer;
     private final ImageLayer settingButtonLayer;
 
     public StartScreen(final ScreenStack ss){
-//        settingScreen = new SettingScreen(ss);
 
-        //gameScreen = new GameScreen(ss);
         this.ss = ss;
 
         Image startImage = assets().getImage("images/StartScreen.png");
         Image startButton = assets().getImage("images/StartButton.png");
-        final Image settingButton = assets().getImage("images/SettingButton.png");
+        Image settingButton = assets().getImage("images/SettingButton.png");
 
         startLayer = graphics().createImageLayer(startImage);
         startButtonLayer = graphics().createImageLayer(startButton);
-        startButtonLayer.setTranslation(436,313);
+        startButtonLayer.setTranslation(430,245);
         settingButtonLayer = graphics().createImageLayer(settingButton);
-        settingButtonLayer.setTranslation(436,387);
+        settingButtonLayer.setTranslation(430,320);
+
+
 
         startButtonLayer.addListener(new Mouse.LayerAdapter(){
             @Override
             public void onMouseUp(Mouse.ButtonEvent event) {
-                ss.push(new GameScreen(ss));
+                ss.push(new SelectScreen(ss));
+            }
+        });
+        settingButtonLayer.addListener(new Mouse.LayerAdapter(){
+            @Override
+            public void onMouseUp(Mouse.ButtonEvent event) {
+                ss.push(new SettingScreen(ss));
             }
         });
 
-/*        settingButtonLayer.addListener(new Mouse.LayerAdapter(){
-            @Override
-            public void onMouseUp(Mouse.ButtonEvent event) {
-                ss.push(settingButtonLayer);
-            }
-        });
-*/
     }
 
     @Override
@@ -56,6 +54,7 @@ public class StartScreen extends Screen {
         this.layer.add(startLayer);
         this.layer.add(startButtonLayer);
         this.layer.add(settingButtonLayer);
+
         keyboard().setListener(new Keyboard.Adapter(){
             @Override
             public void onKeyUp(Keyboard.Event event) {
