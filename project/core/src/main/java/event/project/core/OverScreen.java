683abc1634a339c061cb34d98ac1b9ce61a4f50d
Aphisiit <aphisiit086757@hotmail.com>
2016-05-28 05:26:21 +0000

    Added Game Over Screen and manangement in gameOver Screen.

---
+++ project/core/src/main/java/event/project/core/OverScreen.java
@@ -0,0 +1,60 @@
+package event.project.core;
+
+import static playn.core.PlayN.*;
+
+import playn.core.Image;
+import playn.core.ImageLayer;
+import playn.core.Mouse;
+import playn.core.Pointer;
+import tripleplay.game.Screen;
+import tripleplay.game.ScreenStack;
+
+/**
+ * Created by aphis on 28-May-16.
+ */
+public class OverScreen extends Screen {
+
+    private ScreenStack ss;
+    private StartScreen home;
+    private ImageLayer overLayer;
+    private ImageLayer homeLayer;
+    private ImageLayer restartLayer;
+
+    public OverScreen(final ScreenStack ss){
+        this.ss = ss;
+
+        home = new StartScreen(ss);
+
+        Image overImage = assets().getImage("images/gameOver.png");
+        overLayer = graphics().createImageLayer(overImage);
+
+        Image homeImage = assets().getImage("images/homeButton.png");
+        homeLayer = graphics().createImageLayer(homeImage);
+        homeLayer.setTranslation(100,250);
+
+        Image restartImage = assets().getImage("images/restartButton.png");
+        restartLayer = graphics().createImageLayer(restartImage);
+        restartLayer.setTranslation(350,250);
+
+        homeLayer.addListener(new Mouse.LayerAdapter(){
+            @Override
+            public void onMouseUp(Mouse.ButtonEvent event) {
+                ss.push(new StartScreen(ss));
+            }
+        });
+        restartLayer.addListener(new Mouse.LayerAdapter(){
+            @Override
+            public void onMouseUp(Mouse.ButtonEvent event) {
+                ss.push(new GameScreen(ss));
+            }
+        });
+    }
+
+    @Override
+    public void wasShown() {
+        super.wasShown();
+        this.layer.add(overLayer);
+        this.layer.add(homeLayer);
+        this.layer.add(restartLayer);
+    }
+}
